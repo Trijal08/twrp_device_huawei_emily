baa3153b16066503aaa62db00c48eec8d8a4198b
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-01-16 19:19:57 -0500

    vendorsetup.sh: Manually change maintainer image
    Change maintainer image with a symbolic link. This will work in any OrangeFox version, be it 14.X, 12.X, or even 9.X.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,9 @@
 #export FOX_VERSION="R11.1"
 export OF_MAINTAINER="GamerBoy1234294"
 export MAINTAINER=${OF_MAINTAINER}
-export OF_MAINTAINER_AVATAR="device/huawei/emily/maintainer.png"
+if [ -f $(realpath .)/bootable/recovery/gui/theme/portrait_hdpi/images/Default/About/maintainer.png ]; then
+    ln -sf ../../../../../../../../device/huawei/emily/maintainer.png $(realpath .)/bootable/recovery/gui/theme/portrait_hdpi/images/Default/About/maintainer.png
+fi
 
 # Build environment stuffs
 export ALLOW_MISSING_DEPENDENCIES=true
